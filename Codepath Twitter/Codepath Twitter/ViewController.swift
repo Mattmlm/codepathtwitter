b045a87f2f0b4243519a0bb1b9fd2e181442d2af
Matt Mo <matthew.l.mo@gmail.com>
2015-10-01 13:13:08 -0700

    Create xcode project

---
+++ Codepath Twitter/Codepath Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Codepath Twitter
+//
+//  Created by admin on 10/1/15.
+//  Copyright © 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
