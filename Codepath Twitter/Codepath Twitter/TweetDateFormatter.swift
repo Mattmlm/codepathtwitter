eab9d0e98a5daeec54d5951b0e6bb3db2677f046
Matt Mo <matthew.l.mo@gmail.com>
2015-10-03 00:11:10 -0700

    [Util] - add dateformatter singleton for tweets

---
+++ Codepath Twitter/Codepath Twitter/TweetDateFormatter.swift
@@ -0,0 +1,25 @@
+//
+//  TweetDateFormatter.swift
+//  Codepath Twitter
+//
+//  Created by admin on 10/2/15.
+//  Copyright © 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+class TweetDateFormatter : NSObject {
+    class var sharedInstance : NSDateFormatter {
+        struct Static {
+            static var token : dispatch_once_t = 0
+            static var instance : NSDateFormatter? = nil
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = NSDateFormatter()
+            Static.instance!.dateFormat = "EEE MMM d HH:mm:ss Z y"
+        }
+        
+        return Static.instance!
+    }
+}
